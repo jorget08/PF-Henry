dc07053df12ee694b4122d21442ee6eb0c871cd4
Genes48 <95255005+Genes48@users.noreply.github.com>
2022-05-26 11:46:37 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,15 @@
 import './App.css';
 import React from "react";
-import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
 import ShowBooks from './components/ShowBooks/ShowBooks';
 import BookDetail from "./components/BookDetail/BookDetail"
 
 function App() {
   return (
-    <BrowserRouter>
-    <div>
-      <Switch>
+    <>
       <Route path={"/Home"} component={ShowBooks}/>
       <Route path={"/book/:id"} component={BookDetail}/>
-      </Switch>
-    </div>
-    </BrowserRouter>
+    </>
   );
 }
 
