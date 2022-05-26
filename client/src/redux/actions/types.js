7e248ec679e67ff160d1a0fa349c2fecc7a9ab7c
Genes48 <95255005+Genes48@users.noreply.github.com>
2022-05-25 21:19:33 -0300

    Acciones para filtros y orden
    Se realizaron las acciones para filtrar por categoria(genero), review y precio; las tres esperan una ruta del back que tariga un array filtrado de los libros, a la de precio se le pasa un precio1 y un precio2 para que tengan de parametros para el filtrado, cualquier cosa se hace sino la logica en el front.
    Se realizo la accion para ordenar alfabeticamente los libros por el titulo, en este caso la logica se hace en el reducer, ya que toma la ruta del back que trae todos los libros y la ordena.
    Se cambio el nombre funcion clear usada en el componente Detail por clearDetail para evitar confusiones, y en el mismo componente se corrigio como se renderizaban los generos, ahora se hace un mapeo del array que los trae.

--- client/src/redux/actions/types.js
+++ client/src/redux/actions/types.js
@@ -1,3 +1,7 @@
 export const GET_BY_SEARCH = "GET_BY_SEARCH";
 export const GET_DETAIL = "GET_DETAIL";
 export const CLEAR_DETAIL = "CLEAR_DETAIL";
+export const FILTER_CATEGORY = "FILTER_CATEGORY";
+export const FILTER_REVIEW = "FILTER_REVIEW";
+export const FILTER_PRICE = "FILTER_PRICE";
+export const ORDEN_TITLE = "ORDEN_TITLE";
