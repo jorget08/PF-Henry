7114334cbdfc4949cfd72dfaf95c27ce83a26697
Genes48 <95255005+Genes48@users.noreply.github.com>
2022-05-25 23:56:26 -0300

    Route Book Detail
    Se agrego la ruta a Book Detail en la App

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 import ShowBooks from './components/ShowBooks/ShowBooks';
+import BookDetail from "./components/BookDetail/BookDetail"
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
+    <div>
+      <Switch>
       <Route path={"/Home"} component={ShowBooks}/>
-    </>
+      <Route path={"/book/:id"} component={BookDetail}/>
+      </Switch>
+    </div>
+    </BrowserRouter>
   );
 }
 
