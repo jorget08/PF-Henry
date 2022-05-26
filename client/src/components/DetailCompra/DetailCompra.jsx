54d74b9f5a5a5b6f1ff3acc5e16992986ca8c3f9
Genes48 <95255005+Genes48@users.noreply.github.com>
2022-05-26 16:52:46 -0300

    Update DetailCompra.jsx

--- client/src/components/DetailCompra/DetailCompra.jsx
+++ client/src/components/DetailCompra/DetailCompra.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function Compra({title, author, price, categories, stock}) {
-  var cantidad=[]
-  for(let i=1;i<=stock;i++){
-    cantidad.push(i)
-  }
+export default function Compra({title, author, price, categories}) {
+  
   return (
     <div>
         <h1>{title}</h1>
         <h2>{author}</h2>
         <h3>Género:</h3>{categories.map(e=>{return <h3>{e}</h3>})}
         <h3>{price}</h3>
-        <h4>Cantidad: </h4> <select>{cantidad.map(e=><option value={e}>{e}</option>)}</select>
         <button>Agregar a carrito</button>
         <Link to={"Componente para realizar la compra"}><button>Comprar</button></Link>
     </div>
