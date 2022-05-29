b3e1fe2a997c73c44cf7dc374e870e99c6263c27
CdCordoba <cd.cordoba13@gmail.com>
2022-05-28 23:36:18 -0300

    styles cart

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,36 +1,40 @@
-import React, { useEffect,useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Item from '../Item/Item'
 import NavBar from '../NavBar/NavBar'
 //import { useLocalStore } from '../Utils/useLocalStorage'
-import {removeAllFromCart,getCart} from '../../redux/actions';
-
+import { removeAllFromCart, getCart } from '../../redux/actions';
+import './styles.css'
 export default function Cart() {
 
     const dispatch = useDispatch()
     const bookCarts = useSelector(state => state.cart)
-    const[cart,setCart]= useState([])
-    
-    console.log("soy bookCarts", bookCarts)
-    
+    const [cart, setCart] = useState([])
 
-    function handleDeleteAll(e){
+    console.log("soy bookCarts", bookCarts)
+
+
+    function handleDeleteAll(e) {
         e.preventDefault();
         dispatch(removeAllFromCart())
     }
-     useEffect(() =>{
-    dispatch(getCart())
-    
-     },[dispatch])   
+    useEffect(() => {
+        dispatch(getCart())
 
-    
+    }, [dispatch])
+
+
     return (
-        <>
-        <NavBar/>
-        <button onClick={handleDeleteAll}>Eliminar Todo del Carrito</button>
-        <div>
-            {bookCarts?.map(e => <Item id={e.id} price={"$"+e.price} img={e.image} title={e.title} stock={e.stock} author={e.author} />)}
+        <div className='cartContainer'>
+            <NavBar />
+            <button onClick={handleDeleteAll}>Eliminar Todo del Carrito</button>
+            <div className='items'>
+                {bookCarts?.map(e => <Item id={e.id} price={"$" + e.price} img={e.image} title={e.title} stock={e.stock} author={e.author} />)}
+            </div>
+            <div>
+                <h3>Order Summary</h3>
+                subTotal
+            </div>
         </div>
-        </>
     )
 }
--- client/src/components/Item/Item.jsx
+++ client/src/components/Item/Item.jsx
@@ -1,34 +1,48 @@
 import { React, useState } from 'react'
-import {removeOneFromCart} from '../../redux/actions';
+import { removeOneFromCart } from '../../redux/actions';
 import { useDispatch } from 'react-redux';
-
+import { AiOutlineMinus } from 'react-icons/ai'
+import { MdOutlineAdd } from 'react-icons/md'
+import { FaRegTrashAlt } from 'react-icons/fa'
+import './styles.css'
 export default function Item({ id, title, img, author, price, stock }) {
     const [cant, setCant] = useState(1);
-    const dispatch= useDispatch()
-    
+    const dispatch = useDispatch()
+
     function handlePrice(e) {
         e.preventDefault();
         let newCant = e.target.id === '+' ? cant + 1 : cant - 1
         newCant <= stock ? setCant() : alert("No hay stock")
     }
-    
-    function handleDelete(e){
+
+    function handleDelete(e) {
         e.preventDefault()
         dispatch(removeOneFromCart(id))
     }
     return (
-        <div>
-            <div>
-                <h4>{title}</h4>
-                <h5>{author}</h5>
-                <img src={img} alt="" />
+        <div className='itemContainer'>
+            <div className='itemBook'>
+                <img src={img} alt={title} />
+                <div>
+                    <h4>{title}</h4>
+                    <p>{author}</p>
+
+                </div>
             </div>
-            <div>
+            <div className='prices'>
                 <h5>{price}</h5>
-                <button id='-' onClick={handlePrice}>-</button>
-                <input type="number" value={cant} name="" id="" />
-                <button id='+' onClick={handlePrice}>+</button>
-                <button onClick={handleDelete}>Delete</button>
+                <div className='priceChange'>
+                    <button id='-' onClick={handlePrice}>
+                        <AiOutlineMinus size={20} />
+                    </button>
+                    <input type="number" value={cant} readOnly id="" />
+                    <button id='+' onClick={handlePrice}>
+                        <MdOutlineAdd size={20} />
+                    </button>
+                </div>
+                <button onClick={handleDelete}>
+                    <FaRegTrashAlt size={30} />
+                </button>
             </div>
         </div>
     )
