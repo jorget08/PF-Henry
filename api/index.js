8ef1d7c06f5b3a46f9e60eb1bbbd0bd45c741c9c
Nacho-Thrasher <nachoburgos1995@gmail.com>
2022-05-31 15:18:06 -0300

    a

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const {dataBaseLoad,dataBaseLoadCategories}= require('./src/data/preLoad.js')
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
     dataBaseLoadCategories()
     console.log('categories cargads')
