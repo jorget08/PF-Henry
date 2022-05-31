5d04eb838ffc7abc5610cf0e217840334de1220f
CastilloCamila <97915554+CastilloCamila@users.noreply.github.com>
2022-05-31 15:18:45 -0300

    Update books.js

--- api/src/routes/books.js
+++ api/src/routes/books.js
@@ -3,89 +3,6 @@ const { Book, Category } = require("../db");
 const router = Router();
 const { Op } = require("sequelize");
 
-<<<<<<< HEAD
-
-
-
-
-router.get('/', async (req, res, next) => {
-    try {
-        const { titleOrAuthor, score, rango1, rango2,category } = req.query
-        if (score) {
-            const bookScores= await Book.findAll({
-                where:{
-                    score:score
-                }
-            })
-            return res.json(bookScores)
-
-        }
-        if(rango1 && rango2){
-
-            const bookRange= await Book.findAll({
-                where:{
-                    price:{[Op.between]:[rango1,rango2],
-                    },  
-                },
-                order:[['price','ASC']]
-            })
-            return res.json(bookRange)
-        }
-        if (category) {
-            const bookCategory= await Book.findAll({
-               
-                include:{
-                    model:Category,
-                    where:{
-                        name:category
-                    }
-                }
-            })
-            
-            return res.json(bookCategory)
-        }
-        if (titleOrAuthor) {
-            const bookFound = await Book.findAll({
-                where: {
-                    title: {
-                        [Op.iLike]: `%${titleOrAuthor}%`
-                    }
-                }
-            })
-            const authorFound = await Book.findAll({
-                where: {
-                    author: {
-                        [Op.iLike]: `%${titleOrAuthor}%`
-                    }
-                }
-            })
-
-            let allfound = [].concat(bookFound, authorFound)
-
-            let allfoundResult = [];
-            let lookup = {};
-            let id ='id'
-
-            for (var i in allfound) {
-
-                lookup[allfound[i][id]] = allfound[i];
-            }
-
-            for (i in lookup) {
-                allfoundResult.push(lookup[i]);
-            }
-
-
-            if (allfoundResult.length !== 0) {
-                return res.json(allfoundResult)
-            } else return res.status(400).json({ msg: 'Book with this title or author does not exist' })
-        }
-        const books = await Book.findAll({ include: Category })
-
-        return res.json(books)
-    } catch (error) {
-        next(error)
-=======
 router.get("/", async (req, res, next) => {
   try {
     const { titleOrAuthor, score, rango1, rango2, category } = req.query;
@@ -96,7 +13,6 @@ router.get("/", async (req, res, next) => {
         },
       });
       return res.json(bookScores);
->>>>>>> 14d582b49ecbd555841d597f9148421d3fbda2f9
     }
     if (rango1 && rango2) {
       const bookRange = await Book.findAll({
