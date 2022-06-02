4dfad26c1b3af2cba8c42e01a72b287e09cd441b
Gonzalo Rumi <rumigonzalo@gmail.com>
2022-06-02 13:02:33 -0300

    Upload Support.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import LandingCarousel from "./components/LandingPage/LandingCarousel";
 import LandingPage from './components/LandingPage/LandingPage';
 import Register from './components/Register/Register';
 import UserProfile from "./components/UserProfile/UserProfile";
+import Support from "./components/Support/Support";
 function App() {
   return (
     <>
@@ -24,6 +25,7 @@ function App() {
       <Route path={"/cart"} component={Cart} />
       <Route path={"/register"} component={Register}/>
       <Route path={"/userProfile"} component={UserProfile}/>
+      <Route path={"/support"} component={Support}/>
     </>
   );
 }
