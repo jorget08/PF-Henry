8e5342a1d246a4b269759a27bb1794d0592857ec
AlvaroCordoba-5 <cordobaalvaro16@gmail.com>
2022-06-02 00:40:42 -0300

    update checkot and paypal

--- client/src/components/Checkout/Checkout.jsx
+++ client/src/components/Checkout/Checkout.jsx
@@ -1,34 +1,28 @@
 import React from "react";
-import ReactDOM from "react-dom"
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux'
-import NavBar from '../NavBar/NavBar'
-import Itemscheckout from './Itemscheckout';
-import { getCart,infoBooks,infoSoldBooks } from "../../redux/actions";
-
-
+import ReactDOM from "react-dom";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import NavBar from "../NavBar/NavBar";
+import Itemscheckout from "./Itemscheckout";
+import { getCart, infoBooks, infoSoldBooks } from "../../redux/actions";
 
 const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
 
+export default function Checkout() {
+  const dispatch = useDispatch();
+  const bookCarts = useSelector((state) => state.cart);
+  const TotalPrice = useSelector((state) => state.totalPrice);
+  const infoBook = useSelector((state) => state.infoBooks);
+  const checkoutinfo = JSON.parse(localStorage.getItem("carrito"));
+  console.log("gasdfasdfasf", checkoutinfo);
 
-export default function Checkout(){
-    
-    const dispatch = useDispatch()
-    const bookCarts = useSelector(state => state.cart)
-    const TotalPrice = useSelector(state => state.totalPrice)
-    const infoBook = useSelector(state => state.infoBooks)
+  console.log("soy infoBook", infoBook);
 
-    console.log("soy infoBook",infoBook)
-    
-    useEffect(() => {
-        dispatch(getCart());
-    }, [dispatch])
+  useEffect(() => {
+    dispatch(getCart());
+  }, [dispatch]);
 
-
-    
-
-  const createOrder=(data, actions) =>{
-    
+  const createOrder = (data, actions) => {
     return actions.order.create({
       purchase_units: [
         {
@@ -38,35 +32,33 @@ export default function Checkout(){
         },
       ],
     });
-  }
-  const onApprove=(data, actions)=>{
-    let totalInfo={data:data,totalPrice:TotalPrice,infoBook:infoBook}
-    console.log("soy total info",totalInfo)
-    dispatch(infoBooks(infoBook))
-    dispatch(infoSoldBooks(totalInfo))
+  };
+  const onApprove = (data, actions) => {
+    let totalInfo = { data: data, totalPrice: TotalPrice, infoBook: infoBook };
+    console.log("soy total info", totalInfo);
+    dispatch(infoBooks(infoBook));
+    dispatch(infoSoldBooks(totalInfo));
     return actions.order.capture();
-  }
-  
+  };
 
-    return(
-   <>
-
-   <NavBar></NavBar>
-   {infoBook?.map(e=> <Itemscheckout img={e.image} title={e.title} author={e.author} price={e.price} cant={e.cant}/>)}
-
-<h1>Monto a Pagar:{TotalPrice}</h1>
- 
+  return (
+    <>
+      <NavBar></NavBar>
+      {checkoutinfo?.map((e) => (
+        <Itemscheckout
+          img={e.image}
+          title={e.title}
+          author={e.author}
+          price={e.price * e.cant}
+          cant={e.cant}
+        />
+      ))}
+      <h1>Monto a Pagar:{TotalPrice}</h1>
       <PayPalButton
-        createOrder={(data, actions) =>createOrder(data, actions)}
+        createOrder={(data, actions) => createOrder(data, actions)}
         onApprove={(data, actions) => onApprove(data, actions)}
       />
-    ;
- 
-
-
-
-
-</>
-    )
+      ;
+    </>
+  );
 }
-
--- client/src/components/Checkout/Itemscheckout.jsx
+++ client/src/components/Checkout/Itemscheckout.jsx
@@ -1,27 +1,21 @@
-import React from 'react'
+import React from "react";
 
-
-
-export default function Itemscheckout({title, img, author, price,cant}){
-
-    
-
-return(
+export default function Itemscheckout({ title, img, author, price, cant }) {
+  return (
     <>
-<div className='itemContainer'>
-            <div className='itemBook'>
-                <img src={img} alt={title} />
-                <div>
-                    <h4>{title}</h4>
-                    <p>{author}</p>
-                    <p>Quantity:{cant}</p>
-
-                </div>
-            </div>
-            <div className='prices'>
-                <p>${price}, 00</p>
-                </div>
-                </div>
-  </>
-)
+      <div className="itemContainer">
+        <div className="itemBook">
+          <img src={img} alt={title} />
+          <div>
+            <h4>{title}</h4>
+            <p>{author}</p>
+            <p>Quantity:{cant}</p>
+          </div>
+        </div>
+        <div className="prices">
+          <p>${price}, 00</p>
+        </div>
+      </div>
+    </>
+  );
 }
