cf0615dc9bd6db95af6e86e0ee73ceac865f9fae
Fedegaar <fedegaar@gmail.com>
2022-06-06 10:30:20 -0300

    Update AdminDashBoard

--- client/src/components/AdminDashboard/AdminDashboard.jsx
+++ client/src/components/AdminDashboard/AdminDashboard.jsx
@@ -2,20 +2,14 @@ import React, { useEffect } from 'react'
 import Sidebar from './Sidebar'
 import s from './AdminDashBoard.module.css'
 import Container from './Container'
-import { useDispatch } from 'react-redux'
-import { getBooks, getUsers } from '../../redux/actions'
-
 
 export default function AdminDashboard() {
-
-
-
+  
   return (
     <div className={s.Home}>      
         <Sidebar/>
-
       <div className={s.Container}>
-          <Container/>
+        <Container/>
       </div>  
     </div>
   )
