c48cb4571cc8465c3333ca483f0750daf45533e7
Fedegaar <fedegaar@gmail.com>
2022-06-06 14:24:17 -0300

    Dashboard Updates

--- client/src/components/AdminDashboard/Sidebar.jsx
+++ client/src/components/AdminDashboard/Sidebar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import s from './Sidebar.module.css'
 import {RiHome2Line} from 'react-icons/ri'
+import {BiSupport,BiBookAdd, BiUserCheck, BiTrendingUp} from 'react-icons/bi'
 
 export default function Sidebar() {
     
@@ -10,26 +11,26 @@ export default function Sidebar() {
     <div className={s.sidebar}>
         <ul>
           <hr/>
+          <li className={s.Li}>            
+            <Link to="/admin/" className={s.Li}> <RiHome2Line/> <span style={{marginLeft : "10px"}}> Home</span></Link>
+          </li>
+          <hr />
           <li className={s.Li}>
-            <RiHome2Line/>
-            <Link to="/admin/" className={s.Li}>Home ▶ </Link>
+            <Link to="/admin/newbook" className={s.Li}> <BiBookAdd/> <span style={{marginLeft : "10px"}}> New Book</span></Link>
           </li>
           <hr />
-          <li>
-            <Link to="/admin/newbook" className={s.Li}>New book ▶</Link>
+          <li className={s.Li}>
+            <Link to="/admin/stock" className={s.Li}><BiTrendingUp/> <span style={{marginLeft : "10px"}}> Stock</span></Link>
           </li>
           <hr />
-          <li>
-            <Link to="/admin/stock" className={s.Li}>Stock ▶</Link>
+          <li className={s.Li}>
+            <Link to="/admin/users" className={s.Li}> <BiUserCheck/> <span style={{marginLeft : "10px"}}> Users</span></Link>
           </li>
           <hr />
-          <li>
-            <Link to="/admin/users" className={s.Li}>Users ▶</Link>
+          <li className={s.Li}>
+            <Link to="/admin/support" className={s.Li}> <BiSupport/> <span style={{marginLeft : "10px"}}> Support</span></Link>
           </li>
           <hr />
-          <li>
-            <Link to="/admin/support" className={s.Li}>Support ▶</Link>
-          </li>
         </ul>
     </div>
     </>
