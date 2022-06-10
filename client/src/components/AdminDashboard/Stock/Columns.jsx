a673fd7f37bd55866b3f96a0c2bc4eb911593d36
Fedegaar <fedegaar@gmail.com>
2022-06-10 16:09:49 -0300

    Stock update

--- client/src/components/AdminDashboard/Stock/Columns.jsx
+++ client/src/components/AdminDashboard/Stock/Columns.jsx
@@ -1,3 +1,18 @@
-
-
-
+export const COLUMNS = [
+    {   
+        Header: 'Title',
+        accessor: 'title',
+    },
+    {          
+        Header: 'Author',
+        accessor: 'author'
+    },
+    {        
+        Header: 'Stock',
+        accessor: 'stock',
+    },
+    {        
+        Header: 'Price',
+        accessor: 'price',
+    },
+  ]
