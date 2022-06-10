5ca9c7f064f35affc7477b998b61064ee7422bef
Fedegaar <fedegaar@gmail.com>
2022-06-10 10:59:04 -0300

    Admin sales update.

--- client/src/components/AdminDashboard/Cards/CardB.jsx
+++ client/src/components/AdminDashboard/Cards/CardB.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import s from './CardB.module.css'
+import c from './CardB.module.css'
 import { Link } from 'react-router-dom'
 
 
-export default function Card({books}) {
-    const b = books && books.length;
+export default function CardB ({sales}) {
+    const s = sales && sales.length;
 
    
   return (
-      <Link to='/admin/buys'>
-        <div className={s.Card}>
-            <h5 className={s.h5}>Shopping</h5>
-            <h1 className={s.h1}>{b}</h1>
+      <Link to='/admin/sales'>
+        <div className={c.Card}>
+            <h5 className={c.h5}>Shopping</h5>
+            <h1 className={c.h1}>{s}</h1>
         </div>
       </Link>
    
