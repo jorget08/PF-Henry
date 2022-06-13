d78dee1d44dce2b6c0427690081f6a1cec54bb6c
CastilloCamila <camila.castillo.manrique@gmail.com>
2022-06-13 19:20:39 -0300

    edit Modal y change password estilos

--- client/src/components/UserProfile/UserProfile.jsx
+++ client/src/components/UserProfile/UserProfile.jsx
@@ -3,15 +3,19 @@ import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { getUser, requestPassword, deleteProfile } from '../../redux/actions';
 import NavBar from '../NavBar/NavBar';
-import { AiOutlineDelete } from 'react-icons/ai'
+import Modal from '../Modal/Modal';
+import EditProfile from '../EditProfile/EditProfile'
+import { useModals } from '../Utils/useModals';
 import { FaUserEdit } from 'react-icons/fa'
-import {RiMapPinAddLine} from 'react-icons/ri'
+import {AiOutlineDelete} from 'react-icons/ai'
+import { RiMapPinAddLine } from 'react-icons/ri'
 import { MdOutlinePassword } from 'react-icons/md'
 import Swal from "sweetalert2";
 import './style.css'
 export default function UserProfile() {
   var user = useSelector(state => state.user)
   const dispatch = useDispatch()
+  const [isOpenModal, openModal, closeModal] = useModals(false);
 
   useEffect(() => {
     dispatch(getUser())
@@ -54,15 +58,20 @@ export default function UserProfile() {
 
         <div className='settingsUser'>
 
-          <Link to='/editProfile'>
-            <button title="Edit Profile" >
-              <FaUserEdit color="#c03b3b" size={25} />
-            </button> 
-          </Link>
+
+          <button className='buttonSeting' onClick={openModal} title="Edit Profile" >
+            <FaUserEdit color="#c03b3b" size={25} />
+          </button>
 
 
-          <button onClick={handleClick} >
+
+          <button onClick={handleClick}  className='buttonSeting' >
             <MdOutlinePassword color="#c03b3b" title="Change Password" size={25} />
+            <Modal isOpen={isOpenModal} closeModal={closeModal}>
+              <EditProfile />
+
+            </Modal>
+
           </button>
 
         </div>
@@ -71,30 +80,31 @@ export default function UserProfile() {
         <div className='adress'>
 
 
-          
+
           {
             <ul className='tableAdress'>
               <li className='tableHeader'>
                 <div className='col col-1'><h2>My Addresses</h2> </div>
-                <div className='col col-2 direction'> <RiMapPinAddLine size={30} color="white"/></div>
+                <div className='col col-2 direction'> <RiMapPinAddLine size={30} color="white" /></div>
               </li>
 
               <ul className='scrollRow'>
-              {user.adress &&
-                user.adress.map(e => {
-                  return (
-                    <li className='table-row'>
-                  <div className='col col-1'>
-                    <p key={e}>{e.street} {e.number}, {e.city}, {e.state}, {e.country}</p>
-                   
-                    </div>
-                    <div className='col col-2 direction'><AiOutlineDelete color="#c03b3b" size={30} title="Delete Address" onClick={() => handleDelete(e.idAdress)}/> </div>
-                    </li>
-                )}
-                )
-              }
+                {user.adress &&
+                  user.adress.map(e => {
+                    return (
+                      <li className='table-row'>
+                        <div className='col col-1'>
+                          <p key={e}>{e.street} {e.number}, {e.city}, {e.state}, {e.country}</p>
+
+                        </div>
+                        <div className='col col-2 direction'><AiOutlineDelete color="#c03b3b" size={30} title="Delete Address" onClick={() => handleDelete(e.idAdress)} /> </div>
+                      </li>
+                    )
+                  }
+                  )
+                }
               </ul>
-              
+
             </ul>
           }
 
