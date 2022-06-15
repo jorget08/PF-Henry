b66e6d916eff71bbffd273280db4fd34793beaf3
Nacho-Thrasher <nachoburgos1995@gmail.com>
2022-06-15 11:07:09 -0300

    a

--- api/src/controllers/support.controllers.js
+++ api/src/controllers/support.controllers.js
@@ -2,13 +2,12 @@ const { User, Rol, Support } = require("../db");
 
 //? support comments 
 const createSupport = async (req, res) => {
-    // const uid = req.uid;
     const { name, email, comment } = req.body;
     try {
         const userExist = await User.findOne({ where: { email } });
         if (!userExist) {
-            //? 
             const support = await Support.create({
+                isUser: false,
                 email,
                 name,
                 comment,
@@ -22,14 +21,13 @@ const createSupport = async (req, res) => {
             });1
         }
         const support = await Support.create({
+            isUser: true,
             email: userExist.email,
             name: userExist.name,
             comment,
             date: new Date(),
             status: 0
         });
-        // await support.setUser(uid);
-        //? get support with user
         const supportWithUser = await Support.findOne({
             where: { idSupport: support.idSupport },
             attributes: { exclude: ['createdAt', 'updatedAt'] },
